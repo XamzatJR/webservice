194423eb3201348d1219b8b76f1987791031a87f
Adam <69192973+SkyAdam1@users.noreply.github.com>
2021-02-27 00:04:04 +0300

    fixed registration

--- users/serializer.py
+++ users/serializer.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop("password")
-        user = User(**validated_data)
+        user = CustomUser(**validated_data)
         user.set_password(password)
         user.save()
         return user
