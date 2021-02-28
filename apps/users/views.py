a3eaee3d8aa87e9573d7dad1dd6acd6001498562
Adam <69192973+SkyAdam1@users.noreply.github.com>
2021-03-01 00:22:49 +0300

    update registration
    нужно доделать urls

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,20 @@
 from .serializer import CustomUser, UserSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.generics import (ListCreateAPIView,RetrieveUpdateDestroyAPIView,)
+from rest_framework.permissions import *
+from .models import CustomUser
+
+class UserCreate(ListCreateAPIView):
+    queryset=CustomUser.objects.all()
+    serializer_class=UserSerializer
+    permission_classes=[IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user=self.request.user
+        serializer.save(user=user)
 
 
-class UserCreate(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    queryset=CustomUser.objects.all()
+    serializer_class=UserSerializer
--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "apps.users",
     "apps.projects",
+    'djoser',
 ]
 
 MIDDLEWARE = [
