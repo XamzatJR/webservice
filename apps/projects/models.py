288b38f6dac4427333a705890e8ffc4740ee5f8a
Adam <69192973+SkyAdam1@users.noreply.github.com>
2021-03-04 16:27:27 +0300

    Create model of project

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.conf import settings
 
-# Create your models here.
+from django.db.models.fields import (
+    BooleanField,
+    CharField,
+    DateField,
+    DateTimeField,
+    EmailField,
+    IntegerField,
+    PositiveIntegerField,
+    TextField,
+    URLField,
+)
+from django.db.models.fields.related import ForeignKey
+from django.shortcuts import reverse
+from django.utils.translation import gettext_lazy as _
+
+
+class Project(models.Model):
+    user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+
+    data_create = DateTimeField(_("Время создания"), auto_now_add=True)
+
+    project_name = CharField(_("Название проекта"), max_length=150, db_index=True)
+
+    project_site = URLField(_("Ссылка на сайт проекта"), max_length=200)
+
+    project_description = TextField(_("Описание продукта/сервиса"))
+
+    project_note = CharField(_("Примечание к проекту"), max_length=150, db_index=True)
+
+    project_responsible = CharField(_("Ответственный"), max_length=150, db_index=True)
--- config/urls.py
+++ config/urls.py
@@ -7,10 +7,7 @@ from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version="v1",
-    ),
+    openapi.Info(title="Snippets API", default_version="v1",),
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
