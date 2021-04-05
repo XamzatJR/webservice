809135b2e66a9a8c2770ec2ebf0571f9c7ecaf27
Adam <69192973+SkyAdam1@users.noreply.github.com>
2021-04-05 22:00:51 +0300

    Add Expert Confirmation

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -10,22 +10,11 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login_url"),
     path("logout/", views.LogoutView.as_view(), name="logout_url"),
     path("registration/", views.RegistrationView.as_view(), name="registration_url"),
-    path(
-        "password_reset/", views.PasswordResetView.as_view(), name="password_reset_url"
-    ),
-    path(
-        "password_reset_done/",
-        views.PasswordResetDoneView.as_view(),
-        name="password_reset_done_url",
-    ),
-    path(
-        "password_reset_confirm/<uidb64>/<token>/",
-        views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm_url",
-    ),
-    path(
-        "password_reset_complete/",
-        views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete_url",
-    ),
+    path("experts/", views.Experts.as_view(), name="experts"),
+    path("login_expert/", views.ExpertLoginView.as_view(), name="login_expert_url"),
+    path("expert/<int:pk>", views.ActiveExpert.as_view(), name="active_expert"),
+    path("password_reset/", views.PasswordResetView.as_view(), name="password_reset_url"),
+    path("password_reset_done/", views.PasswordResetDoneView.as_view(), name="password_reset_done_url",),
+    path("password_reset_confirm/<uidb64>/<token>/", views.PasswordResetConfirmView.as_view(), name="password_reset_confirm_url",),
+    path("password_reset_complete/", views.PasswordResetCompleteView.as_view(), name="password_reset_complete_url",),
 ]
