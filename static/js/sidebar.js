deede5ae07db891ee745e55393c5ee79500a49e6
azureswastika <azureswastika@gmail.com>
2021-05-02 18:40:14 +0300

    Add menu close

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -3,3 +3,9 @@ $('.menu-btn').on('click', function (e) {
     $('.menu').toggleClass('menu_active');
     $('.content').toggleClass('content_active');
 })
+
+$('.wrapper').on('click', function (e) {
+    e.preventDefault();
+    $('.menu').removeClass('menu_active');
+    $('.content').removeClass('content_active');
+})
