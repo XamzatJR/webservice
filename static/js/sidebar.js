acaf062b7dda66b6b2dfef01a6463e0a7058142f
azureswastika <azureswastika@gmail.com>
2021-05-02 21:16:59 +0300

    Fix sidebar

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,11 +1,5 @@
 $('.menu-btn').on('click', function (e) {
     e.preventDefault();
     $('.menu').toggleClass('menu_active');
-    $('.content').toggleClass('content_active');
-})
-
-$('.wrapper').on('click', function (e) {
-    e.preventDefault();
-    $('.menu').removeClass('menu_active');
-    $('.content').removeClass('content_active');
+    $('.container').toggleClass('content_active');
 })
