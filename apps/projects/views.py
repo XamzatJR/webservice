8b7e52947d5ee9ca09d4deb4b61664c55a56903f
Deni <azureswastika@gmail.com>
2021-07-27 16:32:29 +0300

    Add tag filter

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -127,7 +127,7 @@ class NIOKRViewSet(ModelViewSet):
     queryset = NiokrProject.objects.all()
     serializer_class = NiokrProjectSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_fields = ["user", "date"]
+    filterset_fields = ["user", "date", "tag"]
     search_fields = ["theme"]
 
 
